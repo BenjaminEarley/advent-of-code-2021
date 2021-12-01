7e6666270cc973d93ada930f7f1c02c0484bf04d
Benjamin Earley <BenjaminEarley92@gmail.com>
2021-12-01 16:20:36 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,22 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun List<Int>.countValueIncreases() = fold(Pair<Int?, Int>(null, 0)) { (previousNumber, count), value ->
+        Pair(value, count + if (previousNumber != null && previousNumber < value) 1 else 0)
+    }.second
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int = input
+        .map { it.toInt() }
+        .countValueIncreases()
+
+    fun part2(input: List<String>): Int = input
+        .map { it.toInt() }
+        .windowed(3)
+        .map { it.sum() }
+        .countValueIncreases()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
